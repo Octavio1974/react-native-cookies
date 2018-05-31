78e5ea75704cfdd7ea1384c02b3e067312d7ebdb
Octavio1974 <octavio.atencio@gmail.com>
2018-05-31 15:55:55 +1000

    Removed invariant check as causing issues with Jest

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const RNCookieManagerAndroid = NativeModules.RNCookieManagerAndroid;
 let CookieManager;
 
 if (Platform.OS === 'ios') {
-    invariant(RNCookieManagerIOS,
-        'react-native-cookies: Add RNCookieManagerIOS.h and RNCookieManagerIOS.m to your Xcode project');
+    // invariant(RNCookieManagerIOS,
+    //     'react-native-cookies: Add RNCookieManagerIOS.h and RNCookieManagerIOS.m to your Xcode project');
     CookieManager = RNCookieManagerIOS;
 } else if (Platform.OS === 'android') {
-    invariant(RNCookieManagerAndroid,
-        'react-native-cookies: Import libraries to android "react-native link react-native-cookies"');
+    // invariant(RNCookieManagerAndroid,
+    //     'react-native-cookies: Import libraries to android "react-native link react-native-cookies"');
     CookieManager = RNCookieManagerAndroid;
 } else {
-    invariant(CookieManager, 'react-native-cookies: Invalid platform. This library only supports Android and iOS.');
+     //invariant(CookieManager, 'react-native-cookies: Invalid platform. This library only supports Android and iOS.');
 }
 
 const functions = [
@@ -29,6 +29,8 @@ const functions = [
 ];
 
 module.exports = {}
-for (var i = 0; i < functions.length; i++) {
-    module.exports[functions[i]] = CookieManager[functions[i]];
+if (CookieManager) {
+    for (var i = 0; i < functions.length; i++) {
+        module.exports[functions[i]] = CookieManager[functions[i]];
+    }
 }
